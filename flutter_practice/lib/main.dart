6c0ea77b7b797fc0c99d65f164a31bd3528f4d06
wirefox21 <wirefox21@gmail.com>
2025-03-05 05:40:35 +0900

    controllerの追加

--- flutter_practice/lib/main.dart
+++ flutter_practice/lib/main.dart
@@ -6,15 +6,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
-        brightness: Brightness.dark,
         colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.deepPurple, brightness: Brightness.dark),
+            seedColor: Colors.deepOrange, brightness: Brightness.dark),
       ),
       home: LoginScreen(),
     );
@@ -22,30 +23,51 @@ class MyApp extends StatelessWidget {
 }
 
 class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _usernameConroller = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameConroller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login")),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("ユーザー名を入力"),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Username",
-                border: OutlineInputBorder(),
+      appBar: AppBar(title: const Text("Login")),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text("Username"),
+              const SizedBox(height: 10),
+              TextField(
+                controller: _usernameConroller,
+                decoration: const InputDecoration(
+                  labelText: "Input your username",
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            CupertinoButton.filled(child: Text("Login"), onPressed: () {}),
-          ],
+              const SizedBox(height: 30),
+              CupertinoButton.filled(
+                child: const Text("Login"),
+                onPressed: () {
+                  String username = _usernameConroller.text;
+                  print("Username : $username");
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
